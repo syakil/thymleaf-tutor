968fb6b89644fc8319e3f8880bad10831a1918a2
syakil <syakil@amanahcorp.co.id>
2024-09-02 13:48:25 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# thymleaf-tutor
---
+++ src/test/java/com/mvctutor/thymleaf/ThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvctutor.thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
